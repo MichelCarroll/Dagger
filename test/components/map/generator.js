672bec85688eb8d16ac2e2fa125ceb4b7016458e
Michel Carroll <carrmi.0@gmail.com>
2016-04-02 16:55:51 -0400

    fixed tests

--- test/components/map/generator.js
+++ test/components/map/generator.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { Map, DungeonMap } from 'components/map/generator'
+import { Map, DungeonBuilder } from 'components/map/generator'
 import { Rect, Point, Vector } from 'common/geometry'
 import { TYPE_ROCK, TYPE_EMPTY } from 'components/map/types'
 import assert from 'assert'
@@ -59,28 +59,30 @@ describe('Map', () => {
 
 describe('Dungeon Map', () => {
   var map = null
+  var builder = null
 
   beforeEach(() => {
-    map = new DungeonMap(5, 10)
+    map = new Map(5, 10)
+    builder = new DungeonBuilder(map)
   })
 
   describe('digging', () => {
     describe('an existing middle tile', () => {
       it('should turn it empty', () => {
-        map.dig(new Point({x:2, y:3}))
+        builder.dig(new Point({x:2, y:3}))
         expect(map.getCellType(new Point({x:2, y:3}))).to.be.equal(TYPE_EMPTY)
       })
     })
     describe('an unexisting tile', () => {
       it('should not throw', () => {
-        map.dig(new Point({x:20, y:30}))
+        builder.dig(new Point({x:20, y:30}))
       })
     })
   })
 
   describe('digging area', () => {
     it('should turn it empty', () => {
-      map.digArea(new Rect(new Point({x:1, y:3}), new Point({x:3, y:5})))
+      builder.digArea(new Rect(new Point({x:1, y:3}), new Point({x:3, y:5})))
       expect(map.getCellType(new Point({x:1, y:3}))).to.be.equal(TYPE_EMPTY)
       expect(map.getCellType(new Point({x:3, y:5}))).to.be.equal(TYPE_EMPTY)
       expect(map.getCellType(new Point({x:0, y:2}))).to.be.equal(TYPE_ROCK)
